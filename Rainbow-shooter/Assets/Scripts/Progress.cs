2ad64170b5e7f65a0df5466aaad897d2a1519315
MikhailSumarokov95 <108895364+MikhailSumarokov95@users.noreply.github.com>
2023-02-26 16:53:05 +0500

    0.33
    mini fix

--- Rainbow-shooter/Assets/Scripts/Progress.cs
+++ Rainbow-shooter/Assets/Scripts/Progress.cs
@@ -7,20 +7,31 @@ public static class Progress
     public static string DefaultWeaponsBought;
     public static string DefaultWeaponsSelected;
 
+    readonly static string weaponsSelected = nameof(weaponsSelected);
+    readonly static string weaponsBought = nameof(weaponsBought);
+    readonly static string money = nameof(money);
+    readonly static string level = nameof(level);
+    readonly static string battlePass = nameof(battlePass);
+    readonly static string grenades = nameof(grenades);
+    readonly static string superGrenades = nameof(superGrenades);
+    readonly static string sensitivity = nameof(sensitivity);
+    readonly static string soundVolume = nameof(soundVolume);
+    readonly static string musicVolume = nameof(musicVolume);
+
     public static void SaveWeaponsSelected(WeaponsSelected weapons)
     {
         Debug.Log(weapons);
         Debug.Log(JsonUtility.ToJson(weapons));
 
-        GSPrefs.SetString("weaponsSelected", JsonUtility.ToJson(weapons).ToString());
+        GSPrefs.SetString(weaponsSelected, JsonUtility.ToJson(weapons).ToString());
         GSPrefs.Save();
     }
 
     public static WeaponsSelected LoadWeaponsSelected()
     {
-        Debug.Log(GSPrefs.GetString("weaponsSelected", DefaultWeaponsSelected));
+        Debug.Log(GSPrefs.GetString(weaponsSelected, DefaultWeaponsSelected));
 
-        return JsonUtility.FromJson<WeaponsSelected>(GSPrefs.GetString("weaponsSelected", DefaultWeaponsSelected));
+        return JsonUtility.FromJson<WeaponsSelected>(GSPrefs.GetString(weaponsSelected, DefaultWeaponsSelected));
     }
 
     public static void SaveWeaponsBought(WeaponsBought weapons)
@@ -28,100 +39,100 @@ public static class Progress
         Debug.Log(weapons);
         Debug.Log(JsonUtility.ToJson(weapons));
 
-        GSPrefs.SetString("weaponsBought", JsonUtility.ToJson(weapons).ToString());
+        GSPrefs.SetString(weaponsBought, JsonUtility.ToJson(weapons).ToString());
         GSPrefs.Save();
     }
 
     public static WeaponsBought LoadWeaponsBought()
     {
-        Debug.Log(GSPrefs.GetString("weaponsBought", DefaultWeaponsBought));
+        Debug.Log(GSPrefs.GetString(weaponsBought, DefaultWeaponsBought));
 
-        return JsonUtility.FromJson<WeaponsBought>(GSPrefs.GetString("weaponsBought", DefaultWeaponsBought));
+        return JsonUtility.FromJson<WeaponsBought>(GSPrefs.GetString(weaponsBought, DefaultWeaponsBought));
     }
 
     public static void SaveMoney(int value)
     {
-        GSPrefs.SetInt("money", value);
+        GSPrefs.SetInt(money, value);
         GSPrefs.Save();
     }
 
     public static int LoadMoney()
     {
-        return GSPrefs.GetInt("money", 0);
+        return GSPrefs.GetInt(money, 0);
     }
 
     public static void SaveLevel(int value)
     {
-        GSPrefs.SetInt("level", value);
+        GSPrefs.SetInt(level, value);
         GSPrefs.Save();
     }
 
     public static int LoadLevel()
     {
-        return GSPrefs.GetInt("level", 1);
+        return GSPrefs.GetInt(level, 1);
     }
 
     public static void SaveBattlePass()
     {
-        GSPrefs.SetInt("battlePass", 1);
+        GSPrefs.SetInt(battlePass, 1);
         GSPrefs.Save();
     }
 
     public static bool LoadBattlePass()
     {
-        return GSPrefs.GetInt("battlePass", 0) == 1;
+        return GSPrefs.GetInt(battlePass, 0) == 1;
     }
 
     public static void SaveGrenades(int value)
     {
-        GSPrefs.SetInt("grenades", value);
+        GSPrefs.SetInt(grenades, value);
         GSPrefs.Save();
     }
 
     public static int LoadGrenades()
     {
-        return GSPrefs.GetInt("grenades", 0);
+        return GSPrefs.GetInt(grenades, 0);
     }
     
     public static void SaveSuperGrenades(int value)
     {
-        GSPrefs.SetInt("superGrenades", value);
+        GSPrefs.SetInt(superGrenades, value);
         GSPrefs.Save();
     }
 
     public static int LoadSuperGrenades()
     {
-        return GSPrefs.GetInt("superGrenades", 0);
+        return GSPrefs.GetInt(superGrenades, 1);
     }
 
     public static void SaveSensitivity(float value)
     {
-        PlayerPrefs.SetFloat("sensitivity", value);
+        PlayerPrefs.SetFloat(sensitivity, value);
     }
 
     public static float LoadSensitivity()
     {
-        return PlayerPrefs.GetFloat("sensitivity", 1);
+        return PlayerPrefs.GetFloat(sensitivity, 1);
     }
 
     public static void SaveVolume(float value)
     {
-        PlayerPrefs.SetFloat("volume", value);
+        PlayerPrefs.SetFloat(soundVolume, value);
     }
 
     public static float LoadVolume()
     {
-        return PlayerPrefs.GetFloat("volume", 1);
+        return PlayerPrefs.GetFloat(soundVolume, 1);
     }
 
     public static void SaveMusicVolume(float value)
     {
-        PlayerPrefs.SetFloat("MusicVolume", value);
+        PlayerPrefs.SetFloat(musicVolume, value);
     }
 
     public static float LoadMusicVolume()
     {
-        return PlayerPrefs.GetFloat("MusicVolume", 1);
+        return PlayerPrefs.GetFloat(musicVolume, 1);
     }
 
     [Serializable]
